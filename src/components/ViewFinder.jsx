6fedac3ff213a5fc040e3f77951e598136011d91
Jacky <jackylai1030@gmail.com>
2026-07-31 13:36:38 -0400

    create viewfinder structure

--- src/components/ViewFinder.jsx
+++ src/components/ViewFinder.jsx
@@ -1,5 +1,12 @@
 export default function ViewFinder(){
     return(
-        <div>ViewFinder Component</div>
+        <div className="viewFinder">
+            <div className="viewfinder-label">
+                VIEW FINDER
+            </div>
+            <div className="screen">
+                READY
+            </div>
+        </div>
     )
 }
