c2cda11998808d87d920660f4d71e83a9fe16c99
Jacky <jackylai1030@gmail.com>
2026-07-31 15:34:05 -0400

    updated and created structure for SearchInput

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,5 +1,13 @@
 export default function SearchInput(){
     return(
-        <div>SearchInput Component</div>
+        <div className="search-input-container">
+            <label htmlFor="search-input">
+                Subject
+            </label>
+
+            <input id="search-input" 
+            type="text"
+            placeholder="Take a picture" />
+        </div>
     )
 }
