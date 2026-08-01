00501ac7c586e943c82ac194ab218e0311f7b01b
Jacky <jackylai1030@gmail.com>
2026-08-01 12:32:32 -0400

    created camera reducer file

---
+++ src/reducers/cameraReducer.js
@@ -0,0 +1,13 @@
+export const initialState = {
+    searchTerm = "", 
+    photos = [],
+    loading: false,
+    error: ""
+};
+
+export function cameraReducer(state, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
