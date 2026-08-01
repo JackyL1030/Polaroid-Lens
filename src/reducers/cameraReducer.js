8ebd1c143837260b25ae0599fef6d35b52badd18
Jacky <jackylai1030@gmail.com>
2026-08-01 12:46:19 -0400

    added camera reducer state transitions

--- src/reducers/cameraReducer.js
+++ src/reducers/cameraReducer.js
@@ -1,12 +1,20 @@
 export const initialState = {
-    searchTerm = "", 
-    photos = [],
+    searchTerm: "", 
+    photos: [],
     loading: false,
     error: ""
 };
 
 export function cameraReducer(state, action) {
   switch (action.type) {
+    case "SEARCH_TERM_CHANGED":
+        return {...state, searchTerm: action.payload}
+    case "SEARCH_STARTED":
+        return {...state, loading: true, error:""}
+    case "SEARCH_SUCCESS":
+        return {...state, loading:false, photos:action.payload}
+    case "SEARCH_FAILED":
+        return {...state, loading:false, error:action.payload}
     default:
       return state;
   }
