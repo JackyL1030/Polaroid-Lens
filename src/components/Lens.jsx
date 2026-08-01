058e2374a0a15832ce57f509a26cfc6dd829922f
Jacky <jackylai1030@gmail.com>
2026-08-01 16:18:22 -0400

    made the lens for Polaroid

--- src/components/Lens.jsx
+++ src/components/Lens.jsx
@@ -1,5 +1,10 @@
-export default function Lens(){
-    return(
-        <div>Lens Component</div>
-    )
+import "../styles/Lens.css";
+
+export default function Lens() {
+  return (
+    <div className="lens">
+      <div className="lens-inner"></div>
+      <div className="lens-highlight"></div>
+    </div>
+  );
 }
