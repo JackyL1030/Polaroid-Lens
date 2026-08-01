615b31145f3855df1c6ca83cfccebe1611a12070
Jacky <jackylai1030@gmail.com>
2026-08-01 10:57:46 -0400

    added useState to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
 import '../src/styles/App.css'
 import Camera from "./components/Camera"
 
 function App() {
+  // What the user typed
+  const [searchTerm, setSearchTerm] = useState("")
+  // Printed Polaroid photos
+  const [photos, setPhotos] = useState([])
+  // Are we currently talking to API
+  const [loading, setLoading] = useState(false);
+  // Did something go wrong?
+  const [error, setError] = useState("")
+
   return(
     <div>
       <Camera />
